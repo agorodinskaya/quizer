e2a4ec9490ee22e44a502f3d25b5d38b4477deee
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-04-20 23:28:30 +1000

    loaders animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,16 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <main>
-      <div className='loading'></div>
-    </main>
+    <div className='loader-container'>
+      <div className='circle'></div>
+      <div className='circle1'></div>
+      <div className='circle2'></div>
+      <div className='circle3'></div>
+      <div className='circle4'></div>
+      <div className='circle5'></div>
+      <div className='circle6'></div>
+      <div className='circle7'></div>
+    </div>
   );
 };
 
