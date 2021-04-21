e36d8072deae34b747c43061ca80dde9cab7dff6
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-04-21 10:01:18 +1000

    loaders animation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## Quizer-app for quiz lovers, more to follow
+
+[demo here](https://admiring-wozniak-16e3f2.netlify.app/)
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import classes from "./Loading.module.css";
 const Loading = () => {
   return (
-    <div className='loader-container'>
-      <div className='circle'></div>
-      <div className='circle1'></div>
-      <div className='circle2'></div>
-      <div className='circle3'></div>
-      <div className='circle4'></div>
-      <div className='circle5'></div>
-      <div className='circle6'></div>
-      <div className='circle7'></div>
-    </div>
+    <main className={classes.LoadingMain}>
+      <div className={classes.LoaderContainer}>
+        <div className={classes.Circle}></div>
+        <div className={classes.Circle1}></div>
+        <div className={classes.Circle2}></div>
+        <div className={classes.Circle3}></div>
+        <div className={classes.Circle4}></div>
+        <div className={classes.Circle5}></div>
+        <div className={classes.Circle6}></div>
+        <div className={classes.Circle7}></div>
+      </div>
+    </main>
   );
 };
 
